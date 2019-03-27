e4b6d98353ea323335e1779046923650882461d8
kimohgeun <kog1059@gmail.com>
2019-03-27 14:14:24 +0900

    React client setup

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GlobalStyle from '../GlobalStyle';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<GlobalStyle />
+				<h1>FLAG</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
