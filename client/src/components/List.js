6ae82b4280beac07fa4ab28656ba7755dcc22f05
kimohgeun <kog1059@gmail.com>
2019-04-14 14:17:34 +0900

    update

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -16,7 +16,7 @@ class List extends Component {
 	clipboard = flag => {
 		const { username } = this.props;
 		let copyText = document.createElement('textarea');
-		const address = `http://localhost:3000/${username}/${flag}`;
+		const address = `https://flag-ohgoodkim.herokuapp.com/${username}/${flag}`;
 		document.body.appendChild(copyText);
 		copyText.value = address;
 		copyText.select();
--- server.js
+++ server.js
@@ -20,6 +20,15 @@ mongoose
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/files', require('./routes/api/files'));
 
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // 서버 생성
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
