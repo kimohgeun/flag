b7d4c5e44cced1ebecc5723f1f6c49d58178bf47
kimohgeun <kog1059@gmail.com>
2019-04-20 02:09:36 +0900

    패키지 업데이트

--- server.js
+++ server.js
@@ -22,8 +22,15 @@ mongoose
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/files', require('./routes/api/files'));
 
-// Serve static assets
-app.use(express.static(path.join(__dirname, 'client / build')));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // 서버 생성
 const port = process.env.PORT || 5000;
